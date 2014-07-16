cf03942acf064d5adbb524aed195be0c78c27324
user <user@MichaeldeMacBook-Pro.local>
2014-07-16 16:55:38 +0800

    test

---
+++ YunGrid/YunGrid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YunGrid
+//
+//  Created by user on 14-7-16.
+//  Copyright (c) 2014年 NCEPU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
